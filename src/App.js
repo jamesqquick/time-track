8addaf48ca8ab389f2c2b7b74321cd4e84c24206
James Quick <james.q.quick@outlook.com>
2018-09-29 11:16:06 -0500

    Added menu button support in navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Navbar from './components/Navbar.jsx';
 class App extends Component {
     render() {
         return (
-            <Navbar title='Time Track' onBack={this.onBack} />
+            <Navbar title='Time Track' onBack={this.onBack} onMenu={this.onMenu} menuItemText="Add" />
         );
     }
 
     onBack = () => {
         console.log("back pressed");
+    }
+
+    onMenu = () => {
+        console.log('menu pressed');
     }
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default (props) => {
         <nav id="nav">
             <div className="menu-btn" id="backBtn" onClick={props.onBack}>{props.onBack ? 'Back' : ''}</div>
             <p>{props.title}</p>
-            <div className="menu-btn" onClick={props.onBack}>{props.onMenu ? 'Menu' : ''}</div>
+            <div className="menu-btn" onClick={props.onMenu}>{props.menuItemText}</div>
         </nav>
     )
 }
