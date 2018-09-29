9fa390de892a6546f1a8d3b92c5e16fb7a533317
James Quick <james.q.quick@outlook.com>
2018-09-29 11:13:51 -0500

    Added back button support in navbar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Navbar from './components/Navbar.jsx';
 class App extends Component {
     render() {
         return (
-            <Navbar title='Time Track' />
+            <Navbar title='Time Track' onBack={this.onBack} />
         );
+    }
+
+    onBack = () => {
+        console.log("back pressed");
     }
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default (props) => {
     return (
         <nav id="nav">
+            <div className="menu-btn" id="backBtn" onClick={props.onBack}>{props.onBack ? 'Back' : ''}</div>
             <p>{props.title}</p>
+            <div className="menu-btn" onClick={props.onBack}>{props.onMenu ? 'Menu' : ''}</div>
         </nav>
     )
 }
