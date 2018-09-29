7839d08f6bae8724d884a5d258a64e2aeddf7380
James Quick <james.q.quick@outlook.com>
2018-09-29 11:05:49 -0500

    Initial commit with scaffolded application and navbar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+
+class App extends Component {
+    render() {
+        return (
+            <Navbar title='Time Track' />
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <nav id="nav">
+            <p>{props.title}</p>
+        </nav>
+    )
+}
